0a9a1ae915f77a1e12031558389d35e120120cf7
luis dourado <lfaddourado@gmail.com>
2023-04-11 14:38:40 -0300

    feat: import posts reducer into store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from './services/postsSlices';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postsReducer,
+  },
 });
