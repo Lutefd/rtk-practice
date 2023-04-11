7b5663bb9ff8ea8c8f2da87feedc33a137393894
luis dourado <lfaddourado@gmail.com>
2023-04-11 15:13:42 -0300

    feat: render post form

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import PostsList from './components/PostsList';
+import PostForm from './components/PostForm';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="grid h-screen w-full place-items-center">
+      <PostForm />
       <PostsList />
     </div>
   );
