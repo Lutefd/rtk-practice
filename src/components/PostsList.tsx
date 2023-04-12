576819e18d222bad0a778fb89959a1fcb6832c77
luis dourado <lfaddourado@gmail.com>
2023-04-12 20:22:00 -0300

    LUT-12 - Render reactions component

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { selectAllPosts } from '../store/services/posts/postsSlice';
 import PostAuthor from './postAuthor';
 import TimeAgo from './createdTime';
+import ReactionButtons from './reactionButtons';
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
@@ -13,7 +14,7 @@ const PostsList = () => {
       <div className="grid gap-2">
         {orderedPosts.map((post) => (
           <div
-            className="card w-64 bg-primary text-primary-content shadow-xl"
+            className="card w-64 bg-primary text-primary-content shadow-xl md:min-w-[30rem]"
             key={post.id}
           >
             <div className="card-body">
@@ -21,6 +22,7 @@ const PostsList = () => {
               <p className="text-start">{post.content}</p>
               <PostAuthor userId={post.userId} />
               <TimeAgo timestamp={post.date} />
+              <ReactionButtons post={post} />
             </div>
           </div>
         ))}
